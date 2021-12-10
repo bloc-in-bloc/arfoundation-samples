3fa0f37497d98447425536a2d52c895b06390d7a
blocovin <kevin@blocinbloc.com>
2021-12-10 17:38:27 +0100

    Add vision

---
+++ Assets/unity-vision/ARKitExample2.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using Possible.Vision.Managed;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+namespace Examples {
+    /// <summary>
+    /// This example demonstrates how to use the Vision and ARKit plugins together.
+    /// </summary>
+    public class ARKitExample2 : MonoBehaviour {
+        public ARCameraManager cameraManager;
+        public Vision vision;
+        public Text uiText;
+
+        private void Awake () {
+            // We need to tell the Vision plugin what kind of requests do we want it to perform.
+            // This call not only prepares the managed wrapper for the specified image requests,
+            // but allocates VNRequest objects on the native side. You only need to call this
+            // method when you initialize your app, and later if you need to change the type
+            // of requests you want to perform. When performing image classification requests,
+            // maxObservations refers to the number of returned guesses, ordered by confidence.
+            vision.SetAndAllocateRequests (VisionRequest.Classification, maxObservations: 1);
+        }
+
+        private void OnEnable () {
+            // Hook up to the completion event of object classification requests
+            vision.OnObjectClassified += VisionOnObjectClassified;
+            if (cameraManager != null) {
+                cameraManager.frameReceived += OnCameraFrameReceived;
+            }
+        }
+
+        private void OnDisable () {
+            vision.OnObjectClassified -= VisionOnObjectClassified;
+            if (cameraManager != null) {
+                cameraManager.frameReceived -= OnCameraFrameReceived;
+            }
+        }
+
+        unsafe void OnCameraFrameReceived (ARCameraFrameEventArgs eventArgs) {
+            // We only classify a new image if no other vision requests are in progress
+            if (vision.InProgress) {
+                return;
+            }
+
+            // Attempt to get the latest camera image. If this method succeeds,
+            // it acquires a native resource that must be disposed (see below).
+            if (!cameraManager.TryAcquireLatestCpuImage (out XRCpuImage image)) {
+                return;
+            }
+
+            XRCpuImage.ConversionParams conversionParams = new XRCpuImage.ConversionParams {
+                // Get the entire image.
+                inputRect = new RectInt (0, 0, image.width, image.height),
+
+                // Downsample by 2.
+                outputDimensions = new Vector2Int (image.width / 2, image.height / 2),
+
+                // Choose RGBA format.
+                outputFormat = TextureFormat.RGBA32,
+
+                // Flip across the vertical axis (mirror image).
+                transformation = XRCpuImage.Transformation.MirrorY
+            };
+            
+            int size = image.GetConvertedDataSize(conversionParams);
+
+            // Allocate a buffer to store the image.
+            var buffer = new NativeArray<byte>(size, Allocator.Temp);
+            var outputBuffer = new IntPtr (buffer.GetUnsafePtr ());
+            
+            try
+            {
+                image.Convert(conversionParams, outputBuffer, buffer.Length);
+            }
+            finally
+            {
+                // We must dispose of the XRCpuImage after we're finished
+                // with it to avoid leaking native resources.
+                image.Dispose();
+            }
+            
+            vision.EvaluateBuffer (outputBuffer, ImageDataType.MetalTexture);
+        }
+
+        private void VisionOnObjectClassified (object sender, ClassificationResultArgs e) {
+            // Display the top guess for the dominant object on the image
+            uiText.text = e.observations.First ().identifier;
+        }
+    }
+}
---
+++ Assets/unity-vision/Possible/Vision/Editor/VisionPostProcess.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+
+namespace Possible.Vision.Editor {
+    public static class VisionPostProcess {
+        [PostProcessBuild]
+        public static void OnPostProcessBuild (BuildTarget buildTarget, string buildPath) {
+            if (buildTarget == BuildTarget.iOS) {
+                PBXProject pbxProject = new PBXProject ();
+                string projectPath = PBXProject.GetPBXProjectPath (buildPath);
+                pbxProject.ReadFromFile (projectPath);
+                
+                string unityFrameworkGuid = pbxProject.TargetGuidByName ("UnityFramework");
+
+                pbxProject.SetBuildProperty(unityFrameworkGuid, "ENABLE_BITCODE", "NO");
+                // string headerPath = pbxProject.FindFileGuidByProjectPath ("Libraries/unity-vision/Scripts/Possible/Vision/Plugins/iOS/Vision-Bridging-Header.h");
+                // if (headerPath == null) {
+                //     Debug.LogError ("Can't find Vision-Bridging-Header.h");
+                // }
+                // pbxProject.AddPublicHeaderToBuild (unityFrameworkGuid, "Libraries/unity-vision/Scripts/Possible/Vision/Plugins/iOS/Vision-Bridging-Header.h");
+                pbxProject.SetBuildProperty(unityFrameworkGuid, "SWIFT_OBJC_INTERFACE_HEADER_NAME", "Vision-Swift.h");
+                pbxProject.AddBuildProperty(unityFrameworkGuid, "LD_RUNPATH_SEARCH_PATHS", "@executable_path/Frameworks $(PROJECT_DIR)/lib/$(CONFIGURATION) $(inherited)");
+                pbxProject.AddBuildProperty(unityFrameworkGuid, "FRAMEWORK_SEARCH_PATHS", "$(inherited) $(PROJECT_DIR) $(PROJECT_DIR)/Frameworks");
+                pbxProject.AddBuildProperty(unityFrameworkGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
+                
+                string stringPbxProject = pbxProject.WriteToString ();
+                File.WriteAllText (projectPath, stringPbxProject);
+                // pbxProject.WriteToFile(buildPath);
+                
+                // proj.SetBuildProperty(targetGuid, "ENABLE_BITCODE", "NO");
+                // proj.SetBuildProperty(targetGuid, "SWIFT_OBJC_BRIDGING_HEADER", "Libraries/Scripts/Possible/Vision/Plugins/iOS/Vision-Bridging-Header.h");
+                // proj.SetBuildProperty(targetGuid, "SWIFT_OBJC_INTERFACE_HEADER_NAME", "Vision-Swift.h");
+                // proj.AddBuildProperty(targetGuid, "LD_RUNPATH_SEARCH_PATHS", "@executable_path/Frameworks $(PROJECT_DIR)/lib/$(CONFIGURATION) $(inherited)");
+                // proj.AddBuildProperty(targetGuid, "FRAMEWORK_SEARCH_PATHS", "$(inherited) $(PROJECT_DIR) $(PROJECT_DIR)/Frameworks");
+                // proj.AddBuildProperty(targetGuid, "ALWAYS_EMBED_SWIFT_STANDARD_LIBRARIES", "YES");
+                // proj.AddBuildProperty(targetGuid, "DEFINES_MODULE", "YES");
+                // // proj.AddBuildProperty(targetGuid, "SWIFT_VERSION", "4.0");
+                // proj.WriteToFile(projPath);
+            }
+        }
+    }
+}
